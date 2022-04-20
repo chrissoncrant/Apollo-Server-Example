0b8ae0a5a1a8aa4bf1483eee08632feb6142f9c5
Chris Soncrant <christopher.soncrant@gmail.com>
2022-04-20 09:49:28 -0700

    set up the server.js file as an Apollo server

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const express = require('express');
+const path = require('path');
 
-const app = express();
+const { ApolloServer } = require('apollo-server-express');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
+const { loadFilesSync } = require('@graphql-tools/load-files');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    `Server listening on port ${PORT}...`
-})
+const typesArray = loadFilesSync(path.join(__dirname, '**/*.graphql'));
+const resolversArray = loadFilesSync(path.join(__dirname, '**/*.resolvers.js'));
+
+async function startApolloServer() {
+    const app = express();
+
+    const schema = makeExecutableSchema({
+        typeDefs: typesArray,
+        resolvers: resolversArray
+    });
+
+    const server = new ApolloServer({
+        schema
+    })
+
+    await server.start();
+
+    server.applyMiddleware({app, path: '/graphql'});
+
+    app.listen(PORT, () => {
+        `Server listening on port ${PORT}...`
+    })
+}
+
+startApolloServer();
+
+
+
+
+
